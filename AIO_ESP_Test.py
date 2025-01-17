09b52063e4cbc2a719ea074663db24edc0d56242
Sasson100 <87619858+Sasson100@users.noreply.github.com>
2025-01-18 00:54:23 +0200

    Create AIO_ESP_Test.py

---
+++ AIO_ESP_Test.py
@@ -0,0 +1,29 @@
+from AIO_ESP32 import AIO_ESP
+from machine import Pin
+wifi_ssid='*wifi name*'
+wifi_password='*wifi password'
+aio_username='*aio username*'
+aio_key='*aio key*'
+aio=AIO_ESP(wifi_ssid,wifi_password,aio_username,aio_key)
+
+
+aio.connect_to_internet()
+def led_callback(msg):
+    if msg is "ON":
+        Pin(2,Pin.OUT).value(1)
+    else:
+        Pin(2,Pin.OUT).value(0)
+# I made this test when I was still making the library, so the temperature feed was just connected to a slider
+def temperature_callback(msg):
+    temperature = float(msg)
+    if temperature<=20:
+        aio.write_feed("temperaturemessage",f"Today it'll be {temperature} degrees, put a jacket on")
+    else:
+        aio.write_feed("temperaturemessage",f"Today it'll be {temperature} degrees, it'll be a nice day today")
+
+aio.register_callback("ledSwitch",led_callback)
+aio.register_callback("temperature",temperature_callback)
+aio.subscribe_feed("ledSwitch")
+aio.subscribe_feed("temperature")
+
+aio.read_feed()
