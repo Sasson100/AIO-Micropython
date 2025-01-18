0b000e2526bc07371595832bf35133c3370ea502
Sasson100 <87619858+Sasson100@users.noreply.github.com>
2025-01-18 02:25:29 +0200

    Update AIO_ESP32.py

--- AIO_ESP32.py
+++ AIO_ESP32.py
@@ -18,27 +18,32 @@ class AIO_ESP:
     
     def connect_to_internet(self):
         # Connecting to wifi
-        wlan = network.WLAN(network.STA_IF)
-        wlan.active(True)
-        wlan.connect(self.ssid, self.password)
+        self.wlan = network.WLAN(network.STA_IF)
+        self.wlan.active(True)
+        self.wlan.connect(self.ssid, self.password)
         print("Connecting to WiFi...", end="")
-        while not wlan.isconnected():
+        while not self.wlan.isconnected():
             time.sleep(1)
             print(".", end="")
         print("\nConnected to WiFi!")
-        print("IP Address:", wlan.ifconfig()[0])
+        print("IP Address:", self.wlan.ifconfig()[0])
         # Connect to Adafruit IO
-        self.client = MQTTClient(self.CLIENT_ID,self.BROKER, user=self.aio_username, password=self.aio_key,keepalive=60)
+        self.client = MQTTClient(self.CLIENT_ID,self.BROKER, user=self.aio_username, password=self.aio_key)
         def def_callback(topic,msg):
             feed_name=topic.decode('utf-8').split("/")[-1]
-            print(f'Received message on the feed {feed_name}: {msg.decode("utf-8")}')
-            if feed_name in self.callbacks:
-                self.callbacks[feed_name](msg.decode('utf-8'))
+            if feed_name is "throttle":
+                print("The data rate has been reached, please lower your feed writes per minute")
             else:
-                print(f'No callback registered for feed "{feed_name}"')
+                print(f'Received message on the feed {feed_name}: {msg.decode("utf-8")}')
+                if feed_name in self.callbacks:
+                    self.callbacks[feed_name](msg.decode('utf-8'))
+                else:
+                    print(f'No callback registered for feed "{feed_name}"')
         self.client.set_callback(def_callback)  # Set the callback for received messages
         self.client.connect()
         print("Connected to Adafruit IO!")
+        self.client.subscribe(f"{self.aio_username}/throttle")
+        
     
     def subscribe_feed(self,feed_name):
         if self.client:
@@ -66,5 +71,6 @@ class AIO_ESP:
                 except Exception as e:
                     print(f'Reading feeds failed with the error:')
                     print(f'{type(e).__name__}: {e}')
+                    time.sleep(5)
         _thread.start_new_thread(check_messages,())
 
